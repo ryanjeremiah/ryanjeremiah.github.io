ba5a6cf71d2fe683d74c3c01cb509dfc6b4ac155
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-03-22 12:56:03 -0700

    Update project_2.js

--- project_2.js
+++ project_2.js
@@ -1,5 +1,6 @@
 let maps: ["there", "here", "nowhere", "gone", "good", "whatsup", "bad"]:
 let decide;
+let timeout;
 
 functin perload(){
   for (let i= 0; i=15; i++){
@@ -18,11 +19,34 @@ function setup(){
   maps.push("1");
 text("click to randomize", 50,50);
     console.log(maps);
+ 
+ setInterval(changeBackground, 1000);
 
+function changeBackground(){
+    if (counter<=50){
+              counter++;
+      counter.log('counter') 
+   background (random(225), random(225),random(225);
+   setInterval(changeBackground, 1000);    
+    
+              
+}
+function preload(){
+  maps = loadImage('https://github.com/ryanjeremiah/ryanjeremiah.github.io/blob/main/Call%20of%20duty%20maps/8123a53f2340c4c4dd0b4b649f95f57f.jpeg');
+  maps = loadIamge('https://github.com/ryanjeremiah/ryanjeremiah.github.io/blob/main/Call%20of%20duty%20maps/BO-2.jpeg');
+  maps = loadIamge('https://github.com/ryanjeremiah/ryanjeremiah.github.io/blob/main/Call%20of%20duty%20maps/apps.3454.70598857662744501.5a74b685-b8f0-4179-808c-c2eb23914446.jpeg');
+  maps = loadIamge('https://github.com/ryanjeremiah/ryanjeremiah.github.io/blob/main/Call%20of%20duty%20maps/images.jpeg');
+  maps = loadIamge('https://github.com/ryanjeremiah/ryanjeremiah.github.io/blob/main/Call%20of%20duty%20maps/mappack.jpeg');
+  maps = loadIamge('https://github.com/ryanjeremiah/ryanjeremiah.github.io/blob/main/Call%20of%20duty%20maps/miami.jpeg');
+  maps = loadIamge('https://github.com/ryanjeremiah/ryanjeremiah.github.io/blob/main/Call%20of%20duty%20maps/mob05.jpeg');
+  }
+    
 function draw(){
   if(animating == true){
     ellise(random width), random(height), random(50,200);
+
 }
+ 
  function clickhere;
   fill(5);
   textAlign(RIGHT);
@@ -30,7 +54,17 @@ function draw(){
   textStyle(BLOD);
   
   text ('What map should we play on Call of Duty');
+  text("click to randomize", width/4, hight/4);
+  createButton("click to randomize");
+  button = createButton("click to randomize");
+  button.mousePressed();
+
+function randomizer(){
+animating = false;
+  if (maps[0]);
+}
   
 function text(){
  text ('What map should we play on Call of Duty');
 }  
+
