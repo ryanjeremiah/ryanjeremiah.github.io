9a1df6b8c1b68725366dc5d7079543178bdd16d2
ryanjeremiah <64881406+ryanjeremiah@users.noreply.github.com>
2021-03-22 11:52:30 -0700

    Create project_2.js

---
+++ project_2.js
@@ -0,0 +1,36 @@
+let maps: ["there", "here", "nowhere", "gone", "good", "whatsup", "bad"]:
+let decide;
+
+functin perload(){
+  for (let i= 0; i=15; i++){
+  Cod(i) = loadImage('assets/cod_#{i}.JPG'}
+}
+function setup(){
+    createCanvas(700,600);
+    background(200);
+    
+  console.log("intianl array is");
+  console.log(maps);
+  
+  maps.pop
+  console.log(maps);
+  
+  maps.push("1");
+text("click to randomize", 50,50);
+    console.log(maps);
+
+function draw(){
+  if(animating == true){
+    ellise(random width), random(height), random(50,200);
+}
+ function clickhere;
+  fill(5);
+  textAlign(RIGHT);
+  textSize(60);
+  textStyle(BLOD);
+  
+  text ('What map should we play on Call of Duty');
+  
+function text(){
+ text ('What map should we play on Call of Duty');
+}  
